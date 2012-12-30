805d815cbe287baded2f0b2b92df79178e857799
mikemackintosh <mike.mackintosh@angrystatic.com>
2012-12-30 00:12:42 -0500

    Adding DefaultWebViewController, views, and error views

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Bakery framework.
+ *
+ * (c) Mike Mackintosh <mike@bakeryframework.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$app = include_once 'boot.php';
+
+/**
+ * Register Twig
+ * 
+ * @Sets $app['twig'];
+ *
+ */
+$app->register(new Bakery\Provider\TwigServiceProvider( array(
+		'twig.path' => __DIR__.'/views',
+)));
+
+
+/**
+ * Set Default Root
+ * 
+ */
+$app->mount("", new \dTRGallery\Controllers\DefaultWebViewController());
+
+
+/**
+ * Run the application
+ */
+$app->run();
+
+/**
+ * END FILE;
+ */
