ef663338a74ddbb857acfc9051923963db8fcc90
mikemackintosh <mike.mackintosh@angrystatic.com>
2013-02-03 01:27:12 -0500

    Added admin functions for album management
    Change-Id: I167cd68a9c6beb8ef3cb97bc1f53e25d66e0f01d

--- src/dTRGallery/Controllers/DefaultAdminWebViewController.php
+++ src/dTRGallery/Controllers/DefaultAdminWebViewController.php
@@ -24,8 +24,47 @@ class DefaultAdminWebViewController implements ControllerProviderInterface{
 		
 		// @TODO: Auto-generated method stub
 		$app->route("/albums", function(RequestManager $request) use ( $app ){
-						
+			
+			return $app['twig']->render("admin/albums.twig", array(
+				"albums" => $app['db']->fetchAll("SELECT * FROM dtr_gallery.albums order by display_order"),
+			));
+			
 		})->bind("admin.albums");
+
+		// @TODO: Auto-generated method stub
+		$app->route("/albums/update", function( RequestManager $request, $response = array() ) use ( $app ){
+		
+			$app['db']->update("dtr_gallery.albums", array($request->post('field') ."=?"), array($request->post('val'),$request->post('albumid')), "id=?");
+				
+			$response['successful'] = true;
+				
+			return json_encode($response);
+		
+		})->bind("admin.album.update");
+		// @TODO: Auto-generated method stub
+		$app->route("/albums/create", function( RequestManager $request, $response = array() ) use ( $app ){
+			$q = $app['db']->fetchAll("SELECT id FROM dtr_gallery.albums where name=?", array($request->post('val')));
+			if(sizeof($q) > 0){
+				$response['successful'] = false;
+			}
+			else{
+				$app['db']->insert("dtr_gallery.albums", array("name=?"), array($request->post('val')));
+				$app['db']->update("dtr_gallery.albums", array("display_order=?"), array($app['db']->lastInsertId(),$app['db']->lastInsertId()), "id=?");
+				$response['successful'] = $app['db']->lastInsertId();
+			}
+			
+			return json_encode($response);
+		
+		})->bind("admin.album.create");
+		
+		// @TODO: Auto-generated method stub
+		$app->route("/albums/{album}", function(RequestManager $request, $album) use ( $app ){
+				
+			return $app['twig']->render("admin/album.edit.twig", array(
+					"album" => $app['db']->fetchAssoc("SELECT * FROM dtr_gallery.albums where name=?", array($album)),
+			));
+				
+		})->regex('album', '.*')->bind("admin.album.edit");
 		
 		
 		/**
